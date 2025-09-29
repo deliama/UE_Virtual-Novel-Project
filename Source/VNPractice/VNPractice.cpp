1cf01465edce9decc51d2529099b84cfb5cb6b21
deliama <107669890+deliama@users.noreply.github.com>
2025-09-29 22:54:20 +0800

    创建并初始化项目

---
+++ Source/VNPractice/VNPractice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VNPractice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VNPractice, "VNPractice" );
